e3a46fbf51f6b027c0ba0dcf9db98548bed29c06
vvitoss <orangekaramel@gmail.com>
2022-03-23 09:55:17 +0300

    upgrade build@test from junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,10 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
@@ -11,29 +13,39 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
+
     }
+
 
     @Test
     public void testRemain1() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1500);
         int expected = 500;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
     @Test
     public void testRemain2() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(3000);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
     @Test
     public void testRemain3() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(999);
         int expected = 1;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testRemain4() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(expected,actual);
+
     }
 }
 
